701dddf03898eef08ce742fa84ff23393ea8db6d
FranciscoLicon <francisco17@ciencias.unam.mx>
2022-06-12 20:08:48 -0500

    Add Linear Search

---
+++ SearchAndSortAlgorithms/Algorithms/SearchAlgorithms.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAndSortAlgorithms.Algorithms
+{
+    /// <summary>
+    /// Implementation of some of the basic search algorithms.
+    /// </summary>
+    public class SearchAlgorithms
+    {
+        /// <summary>
+        /// Linear Search.
+        /// </summary>
+        /// <param name="array">A finite integer array not necessarily ordered.</param>
+        /// <param name="x">The integer to find.</param>
+        /// <returns>True if the interger "x" is in the integer array "array", false otherwise.</returns>
+        public bool LinearSearch(int[] array, int x)
+        {
+            for(var i=0; i<array.Length; i++)
+            {
+                if(array[i] == x)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- SearchAndSortAlgorithms/Program.cs
+++ SearchAndSortAlgorithms/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SearchAndSortAlgorithms.Algorithms;
+
+// ---------------------- Search Algorithms ---------------------
+
+var searchAlgorithms = new SearchAlgorithms();
+var intArray = new int[10] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+
+// Linear Search.
+Console.WriteLine(searchAlgorithms.LinearSearch(intArray, 4)); // False.
+Console.WriteLine(searchAlgorithms.LinearSearch(intArray, 19)); // True
