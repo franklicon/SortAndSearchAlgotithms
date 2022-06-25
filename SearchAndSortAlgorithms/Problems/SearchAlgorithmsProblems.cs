17e0dfc7dab5fd031e6e2a89ee36deb1f84e9015
FranciscoLicon <francisco17@ciencias.unam.mx>
2022-06-25 16:51:33 -0500

    Add cyclic sequence problem

---
+++ SearchAndSortAlgorithms/Problems/SearchAlgorithmsProblems.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAndSortAlgorithms.Problems
+{
+    /// <summary>
+    /// Problems that are solved applying the Binary Search strategy.
+    /// </summary>
+    public class SearchAlgorithmsProblems
+    {
+        /// <summary>
+        /// Cyclic Sequence.
+        /// A sequence S = x0, x1,..., xn is cyclically ordered if the smallest number in
+        /// the sequence is the element xi for some unknown i and the sequence S' is ordered 
+        /// in ascending order with S' = xi, xi+1,...,xn, x0, x1,...,xi-1.
+        /// For example: S = {14, 16, 19, 21, 23, 27, 1, 3, 4, 5, 7, 8, 9, 11, 12, 13} is a ciclically 
+        /// ordered sequence with i = 6.
+        /// </summary>
+        /// <param name="array">A finite interger array cyclically ordered.</param>
+        /// <returns>The index of the smallest element.</returns>
+        public int CyclicSequence(int[] array)
+        {
+            var left = 0;
+            var right = array.Length - 1;
+            while(left <= right)
+            {
+                var mid = (left + right) / 2;
+                if(left == right)
+                {
+                    return left;
+                }
+                if(array[mid] < array[right])
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid+1;
+                }
+            }
+            return default;
+        }
+    }
+}
--- SearchAndSortAlgorithms/Program.cs
+++ SearchAndSortAlgorithms/Program.cs
@@ -1,9 +1,13 @@
 ﻿using SearchAndSortAlgorithms.Algorithms;
+using SearchAndSortAlgorithms.Problems;
 
 // ---------------------- Search Algorithms ---------------------
 
 var searchAlgorithms = new SearchAlgorithms();
+var searchAlgorithmsProblems = new SearchAlgorithmsProblems();
 var intArray = new int[10] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+var cyclicSequenceA = new int[16] { 14, 16, 19, 21, 23, 27, 1, 3, 4, 5, 7, 8, 9, 11, 12, 13 };
+var cyclicSequenceB = new int[9] { 14, 16, 18, 2, 4, 6, 8, 10, 12 };
 
 // Linear Search.
 Console.WriteLine(searchAlgorithms.LinearSearch(intArray, 4)); // False.
@@ -12,3 +16,6 @@ Console.WriteLine(searchAlgorithms.LinearSearch(intArray, 19)); // True
 // Binary Search.
 Console.WriteLine(searchAlgorithms.BinarySearch(intArray, 4)); // False.
 Console.WriteLine(searchAlgorithms.BinarySearch(intArray, 19)); // True
+
+Console.WriteLine(searchAlgorithmsProblems.CyclicSequence(cyclicSequenceA)); //6
+Console.WriteLine(searchAlgorithmsProblems.CyclicSequence(cyclicSequenceB)); //3
