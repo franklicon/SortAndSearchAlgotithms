a12090dbb7cdc65767be0b8365110a0bcc072054
FranciscoLicon <francisco17@ciencias.unam.mx>
2022-06-26 20:57:26 -0500

    Add Bubble Sort

---
+++ SearchAndSortAlgorithms/Algorithms/SortAlgorithms.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAndSortAlgorithms.Algorithms
+{
+    /// <summary>
+    /// Implementation of some sort algorithms.
+    /// </summary>
+    public class SortAlgorithms
+    {
+        /// <summary>
+        /// Bubble Sort.
+        /// </summary>
+        /// <param name="array">A finite interger array.</param>
+        /// <returns>The array "array" in ascending order.</returns>
+        /// Time complexity: O(n^2).
+        public int[] BubbleSort(int[] array)
+        {
+            var length = array.Length - 1;
+            for(var n=0; n<length; n++)
+            {
+                for (var i = 0; i < length - n; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        var temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
--- SearchAndSortAlgorithms/Program.cs
+++ SearchAndSortAlgorithms/Program.cs
@@ -5,17 +5,27 @@ using SearchAndSortAlgorithms.Problems;
 
 var searchAlgorithms = new SearchAlgorithms();
 var searchAlgorithmsProblems = new SearchAlgorithmsProblems();
-var intArray = new int[10] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+var intSearchArray = new int[10] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
 var cyclicSequenceA = new int[16] { 14, 16, 19, 21, 23, 27, 1, 3, 4, 5, 7, 8, 9, 11, 12, 13 };
 var cyclicSequenceB = new int[9] { 14, 16, 18, 2, 4, 6, 8, 10, 12 };
 
 // Linear Search.
-Console.WriteLine(searchAlgorithms.LinearSearch(intArray, 4)); // False.
-Console.WriteLine(searchAlgorithms.LinearSearch(intArray, 19)); // True
+Console.WriteLine(searchAlgorithms.LinearSearch(intSearchArray, 4)); // False.
+Console.WriteLine(searchAlgorithms.LinearSearch(intSearchArray, 19)); // True
 
 // Binary Search.
-Console.WriteLine(searchAlgorithms.BinarySearch(intArray, 4)); // False.
-Console.WriteLine(searchAlgorithms.BinarySearch(intArray, 19)); // True
+Console.WriteLine(searchAlgorithms.BinarySearch(intSearchArray, 4)); // False.
+Console.WriteLine(searchAlgorithms.BinarySearch(intSearchArray, 19)); // True
 
 Console.WriteLine(searchAlgorithmsProblems.CyclicSequence(cyclicSequenceA)); //6
 Console.WriteLine(searchAlgorithmsProblems.CyclicSequence(cyclicSequenceB)); //3
+
+// ---------------------- Search Algorithms ---------------------
+
+var sortAlgorithms = new SortAlgorithms();
+var intArrayToSortA = new int[10] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+var intArrayToSortB = new int[10] { 9, 7, 8, 5, 6, 0, 3, 2, 1, 4 };
+
+// Bubble Sort.
+Console.WriteLine(string.Join(",", sortAlgorithms.BubbleSort(intArrayToSortA)));
+Console.WriteLine(string.Join(",", sortAlgorithms.BubbleSort(intArrayToSortB)));
